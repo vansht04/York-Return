f7acde88e1f56ca8103b313a22b3102765858a65
Vansh Tejnani <vanshbt@gmail.com>
2026-07-15 11:41:57 -0400

    Update team member image URL for Agasti Kulkarni

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -21,7 +21,7 @@ export default function Team() {
       name: "Agasti Kulkarni",
       role: "Founder",
       bio: "LOOKING FOR AN INTERNSHIP, HIRE ME!!!!",
-      img: "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnVubnklMjBjYXR8ZW58MHx8MHx8fDA%3D",
+      img: "https://media.licdn.com/dms/image/v2/D4E03AQGfzSoACMojJg/profile-displayphoto-crop_800_800/B4EZtjntwNJQAM-/0/1766902914497?e=1785974400&v=beta&t=DqB26lIRloVY3YuFIBVlv00QK5AR4QDdgRmqaeSWzDU",
       linkedin: "https://ca.linkedin.com/in/agasti-kulkarni-005191329"
     },
     {
